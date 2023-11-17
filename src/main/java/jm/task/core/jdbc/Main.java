62cf6689a2ab743bb5c7c50153d89c9fed04bb2b
getlis <getprog777@gmail.com>
2023-11-17 08:06:34 +0300

    Solution 2/1/1/4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,11 +1,12 @@
 package jm.task.core.jdbc;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
 
-        UserServiceImpl userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser(  "Thomas", "Anderson", (byte) 25);
         userService.saveUser(  "Truman", "Burbank", (byte) 25);
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
@@ -7,34 +8,34 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    private final UserDaoJDBCImpl USER_DAO_JDBC;
+    private final UserDao USER_DAO;
 
     public UserServiceImpl() {
-        USER_DAO_JDBC = new UserDaoJDBCImpl();
+        USER_DAO = new UserDaoJDBCImpl();
     }
 
     public void createUsersTable() {
-        USER_DAO_JDBC.createUsersTable();
+        USER_DAO.createUsersTable();
     }
 
     public void dropUsersTable() {
-        USER_DAO_JDBC.dropUsersTable();
+        USER_DAO.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        USER_DAO_JDBC.saveUser( name, lastName, age );
+        USER_DAO.saveUser( name, lastName, age );
         System.out.println( "User с именем " + name + " " + lastName + " добавлен в базу данных");
     }
 
     public void removeUserById(long id) {
-        USER_DAO_JDBC.removeUserById( id );
+        USER_DAO.removeUserById( id );
     }
 
     public List<User> getAllUsers() {
-        return USER_DAO_JDBC.getAllUsers();
+        return USER_DAO.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        USER_DAO_JDBC.cleanUsersTable();
+        USER_DAO.cleanUsersTable();
     }
 }
