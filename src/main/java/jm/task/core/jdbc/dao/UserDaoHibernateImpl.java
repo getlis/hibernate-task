77c31e82bfe5ed30cab3cf166a76ea6141188f6d
getlis <getprog777@gmail.com>
2023-11-21 18:00:24 +0300

    + rollback(); - import

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -4,18 +4,16 @@ import jm.task.core.jdbc.model.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
-import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 import jm.task.core.jdbc.util.Util;
-import org.hibernate.Transaction;
 
 public class UserDaoHibernateImpl implements UserDao {
 
 	private final SessionFactory sessionFactory;
 	private final List<User> users;
 
-    public UserDaoHibernateImpl() {
+	public UserDaoHibernateImpl() {
 
 		try {
 			this.sessionFactory = Util.createSessionFactory();
@@ -24,12 +22,11 @@ public class UserDaoHibernateImpl implements UserDao {
 			throw new RuntimeException(e);
 		}
 		this.users = new ArrayList<>();
-    }
+	}
 
 
-    @Override
-    public void createUsersTable() {
-		// Использовать SQl, а не HQL
+	@Override
+	public void createUsersTable() {
 		String qrySql = "CREATE TABLE IF NOT EXISTS users (" +
 				"id BIGINT PRIMARY KEY AUTO_INCREMENT," +
 				"name VARCHAR(256) NOT NULL," +
@@ -39,62 +36,87 @@ public class UserDaoHibernateImpl implements UserDao {
 
 		try ( Session session = sessionFactory.openSession() ) {
 			session.beginTransaction();
-			session.createSQLQuery( qrySql ).executeUpdate();
-			session.getTransaction().commit();
+
+			try{
+				session.createSQLQuery( qrySql ).executeUpdate();
+				session.getTransaction().commit();
+			} catch ( RuntimeException ex ) {
+				session.getTransaction().rollback();
+			}
+
 		}
 	}
 
-    @Override
-    public void dropUsersTable() {
-		// Использовать SQl, а не HQL
+	@Override
+	public void dropUsersTable() {
 		String qrySql = "DROP TABLE IF EXISTS users;";
 
 		try ( Session session = sessionFactory.openSession() ) {
 			session.beginTransaction();
-			session.createSQLQuery( qrySql ).executeUpdate();
-			session.getTransaction().commit();
-		}
-    }
 
-    @Override
-    public void saveUser(String name, String lastName, byte age) {
+			try{
+				session.createSQLQuery( qrySql ).executeUpdate();
+				session.getTransaction().commit();
+			} catch ( RuntimeException ex ) {
+				session.getTransaction().rollback();
+			}
+		}
+	}
+
+	@Override
+	public void saveUser(String name, String lastName, byte age) {
 
 		try ( Session session = sessionFactory.openSession() ) {
 			session.beginTransaction();
-			session.save( new User( name, lastName, age) );
-			session.getTransaction().commit();
-		}
-    }
 
-    @Override
-    public void removeUserById(long id) {
+			try{
+				session.save( new User( name, lastName, age) );
+				session.getTransaction().commit();
+			} catch ( RuntimeException ex ) {
+				session.getTransaction().rollback();
+			}
+		}
+	}
+
+	@Override
+	public void removeUserById(long id) {
+
 		try ( Session session = sessionFactory.openSession() ) {
 			session.beginTransaction();
-			User user = session.get(User.class, id);
-			session.delete(user);
-			session.getTransaction().commit();
-		}
-    }
 
-    @Override
-    public List<User> getAllUsers() {
+			try{
+				User user = session.get(User.class, id);
+				session.delete(user);
+				session.getTransaction().commit();
+			} catch ( RuntimeException ex ) {
+				session.getTransaction().rollback();
+			}
+		}
+	}
+
+	@Override
+	public List<User> getAllUsers() {
 
 		String hql = "FROM User";
 
 		try (Session session = sessionFactory.openSession()) {
 			return session.createQuery(hql, User.class).getResultList();
 		}
-    }
+	}
 
-    @Override
-    public void cleanUsersTable() {
-		// Использовать SQl, а не HQL
+	@Override
+	public void cleanUsersTable() {
 		String qrySql = "TRUNCATE TABLE users;";
 
 		try ( Session session = sessionFactory.openSession() ) {
 			session.beginTransaction();
-			session.createSQLQuery( qrySql ).executeUpdate();
-			session.getTransaction().commit();
+
+			try{
+				session.createSQLQuery( qrySql ).executeUpdate();
+				session.getTransaction().commit();
+			} catch ( RuntimeException ex ) {
+				session.getTransaction().rollback();
+			}
 		}
-    }
+	}
 }
